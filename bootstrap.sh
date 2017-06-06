2d0d9ed6f6fa00605d1c6e198f103f45f269f004
James Slagle <jslagle@redhat.com>
2017-06-06 10:38:56 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git clone https://git.openstack.org/openstack/tripleo-quickstart
-git clone https://git.openstack.org/openstack/tripleo-quickstart-extras
+set -eux
 
+[ -d tripleo-quickstart ] || git clone https://git.openstack.org/openstack/tripleo-quickstart
+[ -d tripleo-quickstart-extras ] || git clone https://git.openstack.org/openstack/tripleo-quickstart-extras
+
+tripleo-quickstart/quickstart.sh --install-deps
 tripleo-quickstart/quickstart.sh --bootstrap
