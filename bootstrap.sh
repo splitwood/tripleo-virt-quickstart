e12fb3f6b896cb44747e8ce8b91e8baf65429f97
James Slagle <jslagle@redhat.com>
2017-06-07 12:04:40 -0400

    Add a check for git and ssh as root to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,20 @@
 
 set -eux
 
+rpm -q git || sudo yum -y install git
+
+function ssh_error {
+    echo "ssh as root failed. Make sure you can ssh as root to $(hostname) without being prompted for a password"
+    exit 1
+
+}
+
+ssh \
+    -o StrictHostKeyChecking=no \
+    -o PasswordAuthentication=no \
+    root@$(hostname) /bin/echo "ssh as root successful" \
+    || ssh_error
+
 [ -d tripleo-quickstart ] || git clone https://git.openstack.org/openstack/tripleo-quickstart
 [ -d tripleo-quickstart-extras ] || git clone https://git.openstack.org/openstack/tripleo-quickstart-extras
 
